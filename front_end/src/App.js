fb1706ecc963d8d699b37ce8f215ffd2a6a26788
phankhaidong <phankhaidong0712@gmail.com>
2022-06-21 19:25:36 +0700

    remove merge mark

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,18 +2,16 @@ import './App.css';
 import SignInWindow from './view/SignIn';
 import SignUpWindow from './view/SignUp';
 import Header from './view/Layout/main';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Routes>
           <Route path="/" element={<Header/>}/>
           <Route path="/user/dang-nhap" element={<SignInWindow/>}/>
           <Route path="/user/dang-ky" element={<SignUpWindow/>}/>
         </Routes>
-      </Router>
     </div>
   );
 }
