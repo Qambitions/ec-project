27809670c602ac880dc30ab6aa04987c19159b6f
phankhaidong <phankhaidong0712@gmail.com>
2022-07-05 22:04:55 +0700

    update payment

---
+++ front_end/src/pages/Payment/VoucherInfo/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+export default function VoucherInfo(props){
+    return(
+        <div className='payment__info_container voucher__info'>
+            <div className="container__flex payment__info_head">
+                <h4>Voucher</h4>
+                <a>Chọn voucher</a>
+            </div>
+            <div className="payment__info_body">
+                <label>Code</label>
+                <label>VC name</label>
+            </div>
+        </div>
+    )
+}
