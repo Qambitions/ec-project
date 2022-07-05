f03fae0e6e5494c378483f41d0c0af3eb71d54e3
Qambitions <63689257+Qambitions@users.noreply.github.com>
2022-07-05 15:33:56 +0700

    finish ads api

--- back_end/app.js
+++ back_end/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountRouter = require('./routes/account.js');
+var ads = require('./routes/ads.js');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/account', accountRouter);
+app.use('/ads', ads);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ back_end/routes/ads.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var knexQuery = require('../db_connect');
+
+var response = {
+    "exitcode": 0,
+    "message": "Lấy dữ liệu quảng cáo thành công",
+    "list_ads": "",
+  }
+
+async function queryAds(props){
+    var date = new Date();
+    var date_str = String(date.toISOString().split('T')[0]);
+    const rawSQL = `  SELECT hinh_anh_qc,vi_tri,link_website FROM hop_dong_quang_cao 
+                      WHERE tg_bat_dau <= '${date_str}'
+                      AND '${date_str}'<= tg_ket_thuc
+                    `
+    // return knexQuery.select().from("store_admin");
+    const result = await knexQuery.raw(rawSQL)
+    console.log(result)
+    return result.rows
+}
+
+router.get('/', async (req, res, next) =>{
+    const retAds = await queryAds(req.body);
+    response.list_ads = retAds
+    res.send(response)
+});
+
+module.exports = router;
