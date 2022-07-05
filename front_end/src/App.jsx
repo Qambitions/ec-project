1aebe1577ece87ecc0f6d114e62e9da3d7570145
phankhaidong <phankhaidong0712@gmail.com>
2022-07-05 21:59:31 +0700

    update route

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -5,17 +5,12 @@ import { Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Cart from './pages/Cart';
 import ErrorPage from './pages/Error';
+import Pages from './pages';
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<Homepage/>}/>
-          <Route path="/user/dang-nhap" element={<SignInWindow/>}/>
-          <Route path="/user/dang-ky" element={<SignUpWindow/>}/>
-          <Route path="/gio-hang" element={<Cart/>}/>
-          <Route path="/error" element={<ErrorPage/>}/>
-        </Routes>
+        <Pages/>
     </div>
   );
 }
