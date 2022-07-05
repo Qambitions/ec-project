986c694f78cad56fb3a2df244d054578c18085cf
phankhaidong <phankhaidong0712@gmail.com>
2022-07-05 22:00:20 +0700

    update header

--- front_end/src/components/Header/index.jsx
+++ front_end/src/components/Header/index.jsx
@@ -1,8 +1,11 @@
 import logo from '../../assets/logo.png';
-import {Navigate} from 'react-router-dom'
+import {Navigate, Link} from 'react-router-dom'
 import React from 'react';
-import { BsCart2,BsBell,BsHouseDoor,BsPersonFill} from "react-icons/bs";
+import { BsCart2,BsBell,BsHouseDoor,BsPersonFill,BsQuestionCircle} from "react-icons/bs";
 import SearchBar from '../SearchBar';
+import './style.css'
+
+
 
 export default function Header(){
     const [goToSignIn, setGoToSignIn] = React.useState(false);
@@ -14,11 +17,20 @@ export default function Header(){
         <div className="container">
             <img className="logo" src={logo} alt={logo}></img>
             <SearchBar/>
-            <div className="nav-signin-menu">
-                <BsHouseDoor/>
-                <BsBell/>
-                <BsCart2/>
-                <a href="#ptt"onClick={()=>{setGoToSignIn(true);}}><BsPersonFill/>Đăng nhập/ Đăng ký</a>
+            <div className="head__nav_signin_menu">
+                <a href='/'><BsQuestionCircle/>Cần giúp đỡ</a>
+                <a href='/'><BsHouseDoor/>Hệ thống cửa hàng</a>
+                <div><BsBell/></div>
+                <a href='/gio-hang'><BsCart2/></a>
+                <a href="#ptt"onClick={()=>{setGoToSignIn(true);}}>
+                    <div className='container__flex'>
+                        <BsPersonFill/>
+                        <div className='container__flex_col'>
+                            <label>Đăng nhập/</label>
+                            <label>Đăng ký</label>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     )
