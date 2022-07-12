e1e79241dabb386664e3708b5c2dcd42a1d65644
Qambitions <63689257+Qambitions@users.noreply.github.com>
2022-07-12 23:31:12 +0700

    add total api for order overview

--- back_end/routes/management/order_overview.js
+++ back_end/routes/management/order_overview.js
@@ -12,10 +12,20 @@ async function queryOrderOverview(props){
   return result.rows  
 }
 
+async function queryTotalOrder(props){
+    const rawSQL = `SELECT count(*)
+                    FROM don_hang dh 
+                  `
+
+  const result = await knexQuery.raw(rawSQL)
+  return result.rows[0].count
+}
+
 router.get('/', async (req, res, next) =>{
     var response = {
         "exitcode": 1,
         "message": "",
+        "total":"",
         "list_order":"",
     }
     if (req.headers.magic_pass != 'LamZauKhumKho'){
@@ -23,10 +33,14 @@ router.get('/', async (req, res, next) =>{
         res.send(response)
         return
     }
+    req.body.limit = (typeof req.body.limit === 'undefined') ? 5 : req.body.limit;
+    req.body.offset = (typeof req.body.offset === 'undefined') ? 0 : req.body.offset;
     const orderOverview = await queryOrderOverview(req.body);
+    const totalOrder = await queryTotalOrder(req.body);
     response.exitcode = 0
     response.message = "lấy thông tin thành công"
-    response.list_order = orderOverview(req.body)
+    response.list_order = orderOverview
+    response.total  =  totalOrder
     res.send(response)
     
 });
