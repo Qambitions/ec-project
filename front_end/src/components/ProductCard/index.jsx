6bc46a04d26b2ed7692f3e41885947a042a15698
phankhaidong <phankhaidong0712@gmail.com>
2022-07-17 11:55:32 +0700

    fetch data product card

--- front_end/src/components/ProductCard/index.jsx
+++ front_end/src/components/ProductCard/index.jsx
@@ -1,31 +1,44 @@
-import './style.css'
-import pd from '../../assets/logo.png'
-import {AiFillStar,AiOutlineShoppingCart} from "react-icons/ai";
-import { ProductDetail } from '../ProductDetail';
-export default function ProductCard(props){
-    return(     
-        <a href='/products' className='product__card'>
-        <img className='product__card_img' src={pd} alt ={pd}/>
-        <div>
-            <div className='product__card_price'>
-                <label className='product__card_price_left'>price</label>
-                <label className='product__card_price_right'>price<span>%</span></label>
-            </div>
-            <h5>BRAND</h5>
-            <p>Product name</p>
-            <div className='product__card_info'>
-                <div>
-                    <AiFillStar className='product__card_rating_star'/>
-                    <AiFillStar className='product__card_rating_star'/>
-                    <AiFillStar className='product__card_rating_star'/>
-                    <AiFillStar className='product__card_rating_star'/>
-                    <AiFillStar className='product__card_rating_star'/>
-                    <span>()</span>
-                    <span> | <AiOutlineShoppingCart/></span>
-                    <span>instock</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-    )
+import "./style.css";
+import pd from "../../assets/logo.png";
+import { AiFillStar, AiOutlineShoppingCart } from "react-icons/ai";
+import { ProductDetail } from "../ProductDetail";
+import { Link } from "react-router-dom";
+
+export default function ProductCard(props) {
+  const obj = props.obj;
+
+  return (
+    <Link to={`/product/${obj.masp}`} className="product__card">
+      <img
+        className="product__card_img"
+        src={obj.hinh_anh}
+        alt={obj.hinh_anh}
+      />
+      <div>
+        <div className="product__card_price">
+          <label className="product__card_price_left">{obj.gia_ban}</label>
+          <label className="product__card_price_right">
+            {obj.gia_ban_giam}
+            <span>{obj.phan_tram_giam_gia}%</span>
+          </label>
+        </div>
+        <h5>BRAND</h5>
+        <p>{obj.ten_sp}</p>
+        <div className="product__card_info">
+          <div>
+            <AiFillStar className="product__card_rating_star" />
+            <AiFillStar className="product__card_rating_star" />
+            <AiFillStar className="product__card_rating_star" />
+            <AiFillStar className="product__card_rating_star" />
+            <AiFillStar className="product__card_rating_star" />
+            <span>({obj.sao.avg})</span>
+            <span>
+              | <AiOutlineShoppingCart />
+            </span>
+            <span>{obj.tong_da_ban}</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 }
