caca43ee0737398db52f5fc46d3f9587d7975975
Qambitions <63689257+Qambitions@users.noreply.github.com>
2022-07-17 12:49:35 +0700

    finish GHN-price api

--- back_end/routes/cart/delivery_price.js
+++ back_end/routes/cart/delivery_price.js
@@ -3,8 +3,6 @@ var axios = require('axios');
 var router = express.Router();
 var knexQuery = require('../../db_connect');
 
-
-
 async function checkClient(props){
     const rawSQL = ` 
                     select *
@@ -17,7 +15,57 @@ async function checkClient(props){
     return result.rows[0]
 }
 
-router.get('/GHN/', async (req, res, next) =>{
+async function queryBranch(){
+    const rawSQL = ` 
+                    select *
+                    from CHI_NHANH
+
+                    `
+    // return knexQuery.select().from("store_admin");
+    const result = await knexQuery.raw(rawSQL)
+    return result.rows
+}
+
+async function getMinPriceGHN(listBranch, dia_chi, sum_weight){
+    var axios_connect = await axios.create({
+        baseURL:'https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee?shop_id=3094149'
+    });
+    var minPrice = {'total_price':1000000,
+                    'macn'       :0}
+    for(var method=53320; method<=53321;method++){
+        for(var i = 0; i < listBranch.length;i++){
+            var price = await axios_connect('/',{
+                method: 'POST',
+                headers: {
+                    token: '3f4f34d9-0136-11ed-b824-262f869eb1a7'
+                },
+                data:{
+                    "service_id":method,
+                    "insurance_value":500000,
+                    "coupon": null,
+                    "from_district_id":parseInt(listBranch[i].districtid),
+                    "to_district_id":parseInt(dia_chi.districtid),
+                    "weight":sum_weight,
+                }
+            }) 
+            .then (function (response) {
+                return response.data.data.total
+            })
+            .catch(function (error) {
+                // console.log(error)
+                return 1000000
+            });
+            // console.log(price)
+            if (minPrice.total_price >  price){
+                minPrice.total_price = price
+                minPrice.macn        = listBranch[i].macn
+            }
+        }
+    }
+    return minPrice
+}
+
+router.post('/GHN/', async (req, res, next) =>{
     var response = {
         "exitcode": 1,
         "message": "Tạo đơn hàng thất bại",
@@ -31,28 +79,9 @@ router.get('/GHN/', async (req, res, next) =>{
         res.send(response)
         return
     }
-
-    var axios_connect = await axios.create({
-        baseURL:'https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee?shop_id=3094149'
-    });
-    var price = await axios_connect('/',{
-        method: 'POST',
-        headers: {
-            token: '3f4f34d9-0136-11ed-b824-262f869eb1a7'
-        },
-        data:{
-            "service_id":53320,
-            "insurance_value":500000,
-            "coupon": null,
-            "from_district_id":1542,
-            "to_district_id":3440,
-            "weight":1000
-        }
-    });
-    console.log(price.data)
-
-
-    
+    const listBranch = await queryBranch();
+    const minPrice = await getMinPriceGHN(listBranch, req.body.dia_chi, req.body.sum_weight);    
+    console.log(minPrice);
     res.send(response)
 });
 
