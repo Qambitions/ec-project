1b4eaf7b544687cb93ce841d25749168c84d545e
thisislins <lephuonglinh169@gmail.com>
2022-08-06 12:16:00 +0700

    Fix display of sidebar

--- front_end/src/components/sidebar/Sidebar.jsx
+++ front_end/src/components/sidebar/Sidebar.jsx
@@ -3,12 +3,40 @@ import {useState} from 'react';
 import {MdDashboard} from "react-icons/md";
 import {FiUsers} from "react-icons/fi";
 import {BsBoxSeam, BsNewspaper} from "react-icons/bs";
+import {RiCoupon2Line} from "react-icons/ri";
 import Logo from "../../assets/logo.png";
 import { Link} from "react-router-dom";
-import {Layout, Menu } from "antd";
-const { Header, Content, Footer, Sider } = Layout;
 
 export default function Sidebar() {
+  const dashboard = [
+    {
+      name: "Main",
+      logo: <MdDashboard/>,
+      path: "/admin"
+  },
+  {
+    name: "Users",
+    logo: <FiUsers/>,
+    path: "/admin/user"
+  },
+  {
+    name: "Stock",
+    logo: <BsBoxSeam/>,
+    path: "/admin/stock"
+  },
+  {
+    name: "Orders",
+    logo: <BsNewspaper/>,
+    path: "/admin/order"
+  },
+  {
+    name: "Vouchers",
+    logo: <RiCoupon2Line/>,
+    path: "/admin/voucher"
+  },
+  
+];
+
   const [isActive, setIsActive] = useState(false);
 
   const handleClick =() => {
@@ -22,33 +50,19 @@ export default function Sidebar() {
       <hr />
       <div className="center">
         <ul>
-          <p className="title">MAIN</p>
-          <Link to="/admin" style={{ textDecoration: "none" }}>
-          <li>
-            <MdDashboard className="fa-3x" style={{color: "black"}}/>
-            <span>Dashboard</span>
-          </li>
-          </Link>
-          
-          <p className="title">LISTS</p>
-          <Link to="/admin/user" style={{ textDecoration: "none" }}>
-            <li>
-              <FiUsers className="fa-3x" style={{color: "black"}}/>
-              <span>Users</span>
-            </li>
-          </Link>
-          <Link to="/admin/stock" style={{ textDecoration: "none" }}>
-            <li>
-              <BsBoxSeam className="fa-3x" style={{color: "black"}}/>
-              <span>Stock</span>
-            </li>
-          </Link>
-          <Link to="/admin/order" style={{ textDecoration: "none" }}>
-          <li>
-            <BsNewspaper className="fa-3x" style={{color: "black"}}/>
-            <span>Orders</span>
-          </li>
-          </Link>    
+        {dashboard.map((item, index) => {
+          return (
+            <>
+            <Link to={item.path} style={{ textDecoration: "none" }}>
+              <li>
+                <div className="fa-3x" style={{color: "black"}}>{item.logo}</div>
+                <span>{item.name}</span>
+              </li>
+            </Link>
+            </>
+
+          )
+      })}
         </ul>
       </div>
     </div>
