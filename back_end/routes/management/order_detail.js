75bb9bc45be6d7ce10ed2a8c886ccd60c0989ed2
Qambitions <63689257+Qambitions@users.noreply.github.com>
2022-08-06 08:48:24 +0700

    fix bug change_status (misunderstand with BA)

--- back_end/routes/management/order_detail.js
+++ back_end/routes/management/order_detail.js
@@ -61,12 +61,22 @@ async function checkFlow(props){
       }
   }
 
+  if (props.trang_thai_hien_tai.toUpperCase().normalize() == 'CHỜ XÁC NHẬN'.normalize() &&
+      (props.trang_thai_moi.toUpperCase().normalize() == 'HỦY ĐƠN HÀNG'.normalize()
+      || props.trang_thai_moi.toUpperCase().normalize() == 'ĐÃ HỦY'.normalize()))
+        return true
+
   if (props.trang_thai_hien_tai.toUpperCase().normalize() == 'ĐÃ XÁC NHẬN'.normalize() &&
-      (props.trang_thai_moi.toUpperCase().normalize() == 'ĐANG GIAO'.normalize() || props.trang_thai_moi.toUpperCase() == 'HỦY ĐƠN HÀNG'.normalize()))
+      (props.trang_thai_moi.toUpperCase().normalize() == 'ĐANG GIAO'.normalize() 
+      || props.trang_thai_moi.toUpperCase().normalize() == 'HỦY ĐƠN HÀNG'.normalize()
+      || props.trang_thai_moi.toUpperCase().normalize() == 'ĐÃ HỦY'.normalize()))
         return true
 
   if (props.trang_thai_hien_tai.toUpperCase().normalize() == 'ĐANG GIAO'.normalize() &&
-      (props.trang_thai_moi.toUpperCase().normalize() == 'ĐÃ GIAO THÀNH CÔNG'.normalize() || props.trang_thai_moi.toUpperCase().normalize() == 'HỦY ĐƠN HÀNG'.normalize()))
+      (props.trang_thai_moi.toUpperCase().normalize() == 'ĐÃ GIAO THÀNH CÔNG'.normalize() 
+        || props.trang_thai_moi.toUpperCase().normalize() == 'HỦY ĐƠN HÀNG'.normalize()
+        || props.trang_thai_moi.toUpperCase().normalize() == 'ĐÃ GIAO'.normalize()
+        || props.trang_thai_moi.toUpperCase().normalize() == 'ĐÃ HỦY'.normalize()))
         return true
 
   return false
