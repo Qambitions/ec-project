295765c73f9d9bd564d20fcf3d9235a21e64db4e
Qambitions <63689257+Qambitions@users.noreply.github.com>
2022-08-13 10:32:38 +0700

    setup for deploy

--- back_end/db_connect.js
+++ back_end/db_connect.js
@@ -1,16 +1,30 @@
 require("dotenv").config();
+const config = require('./config')
+
+const connection_deploy = {
+  host : process.env.PGHOST,
+  port : process.env.PGPORT,
+  user : process.env.PGUSER,
+  password : process.env.PGPASSWORD,
+  database : process.env.PGDATABASE,
+  ssl: { rejectUnauthorized:false },
+}
+
+const connection_dev = {
+  host : process.env.DEV_PGHOST,
+  port : process.env.DEV_PGPORT,
+  user : process.env.DEV_PGUSER,
+  password : process.env.DEV_PGPASSWORD,
+  database : process.env.DEV_PGDATABASE,
+}
 
 const knex = require('knex')({
     client: 'pg',
-    connection: {
-      host : process.env.PGHOST,
-      port : process.env.PGPORT,
-      user : process.env.PGUSER,
-      password : process.env.PGPASSWORD,
-      database : process.env.PGDATABASE
-    },
+    connection: (config.DEV) ? connection_dev :connection_deploy,
     pool: { min: 0, max: 10 }
   });
 
-  module.exports =  knex;
+console.log((config.DEV) ? connection_dev : connection_deploy)
+console.log(config.DEV)
+module.exports =  knex;
  
