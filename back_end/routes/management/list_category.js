9057f00f4304983205307665898df29e74ee7365
Qambitions <63689257+Qambitions@users.noreply.github.com>
2022-08-15 23:45:44 +0700

    finish list category

---
+++ back_end/routes/management/list_category.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var knexQuery = require('../../db_connect');
+
+async function queryCategory(props){
+    var rawSQL = `SELECT *
+                    FROM loai_hang  
+                  `
+  const result = await knexQuery.raw(rawSQL).catch(error => {
+    console.log(error)
+    throw new Error(error);
+  });
+  return result.rows  
+}
+
+router.get('/', async (req, res, next) =>{
+    var response = {
+        "exitcode": 1,
+        "message": "Lấy thông tin thất bại",
+        "chi_nhanh":"",
+    }
+    if (req.headers.magic_pass != 'LamZauKhumKho'){
+        response.message = "sai Pass ròi!!"
+        res.send(response)
+        return
+    }
+    
+    try{
+      const category = await queryCategory(req.query);
+      response.exitcode = 0
+      response.message = "lấy thông tin thành công"
+      response.chi_nhanh = category
+    }
+    catch (e){
+      response.exitcode= 1
+      response.message = e
+      response['warning'] = "có lỗi bất ngờ xảy ra..."
+    }
+
+    return res.send(response)
+    
+});
+
+module.exports = router;
