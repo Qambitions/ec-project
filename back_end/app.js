a334bdcd9cbc8a051038c2b455dce282a5f90c75
Qambitions <63689257+Qambitions@users.noreply.github.com>
2022-08-22 17:15:38 +0700

    Update app.js

--- back_end/app.js
+++ back_end/app.js
@@ -26,24 +26,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use(function (req, res, next) {
+if (typeof(process.env.KEY_PEM_PATH) != "undefined"){
+  app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_HOST);
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_HOST);
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+    // Pass to next layer of middleware
+    next();
+  });
+}
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
